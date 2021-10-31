8ae20e40c6d0c9a385b3a6fa8357c3aa852704d0
bmarquescost <b.marquescost@gmail.com>
2021-10-31 17:30:33 -0300

    Updating main to test bst

--- main.c
+++ main.c
@@ -1,25 +1,36 @@
-#include "queue.h"
+#include "binary_search_tree.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    QUEUE *new_queue = queue_create();
+    BST *bst = bst_create();
     
-    for(int i = 0; i < 10; ++i) 
-        queue_enqueue(new_queue, element_create(i));
+    bst_insert(bst, element_create(10), ITERATIVE);
+    bst_insert(bst, element_create(5), ITERATIVE);
+    bst_insert(bst, element_create(15), ITERATIVE);
+    bst_insert(bst, element_create(2), ITERATIVE);
+    bst_insert(bst, element_create(1), ITERATIVE);
+    bst_insert(bst, element_create(3), ITERATIVE);
+    bst_insert(bst, element_create(7), ITERATIVE);
+    bst_insert(bst, element_create(12), ITERATIVE);
+    bst_insert(bst, element_create(20), ITERATIVE);
+
     
     printf("DEBUG\n");
-    queue_print(new_queue);
+    bst_print(bst, PRE_ORDER);
+    bst_print(bst, IN_ORDER);
+    bst_print(bst, POST_ORDER);
 
     printf("Deleting\n");
-    free(queue_dequeue(new_queue));
-    queue_print(new_queue);
+    bst_delete(bst, 5);
+    bst_print(bst, PRE_ORDER);
+    
+    bst_delete(bst, 3);
+    bst_print(bst, PRE_ORDER);
+    
+    bst_delete(bst, 10);
+    bst_print(bst, PRE_ORDER);
 
-    for (int i = 0; i < 5; ++i) {
-        free(queue_dequeue(new_queue));
-        queue_print(new_queue);
-    }
-
-    queue_delete(&new_queue);    
+    bst_destroy(&bst);    
     return 0;
 }
